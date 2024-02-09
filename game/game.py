a2a76b58305b53e7fff78faed1f2502825a0e95d
oliver earith <100880578+lavamonster09@users.noreply.github.com>
2024-02-09 11:27:55 +0000

    asbjhasgdasdsa

--- game/game.py
+++ game/game.py
@@ -4,30 +4,13 @@ from misc.constants import *
 from screens.screen import Screen
 import game.level as level
 import pygame
-from pygame import gfxdraw
 
 class Game(Screen):
     def __init__(self, screen_manager):
         super().__init__(screen_manager)
-        self.back_color = DARK_BACKGROUND_COLOR
-        self.game_surface = pygame.surface.Surface((SCREEN_WIDTH / 1.6, SCREEN_HEIGHT / 1.6))
-        self.game_surface.set_colorkey((0,0,0))
-        self.generator = level.Generator((0, SCREEN_WIDTH / 1.6), (0, SCREEN_HEIGHT / 1.6))
-        self.points = self.generator.generate_random_path(6)
-        
+        self.level_manager = level.Level_manager(1.6)
+    
     def draw(self):
-        self.game_surface.fill((0,0,0))
         super().draw()
-        for i in range(len(self.points)):
-            pygame.draw.circle(self.game_surface,(255,255,255),self.points[i],2)
-        for x in range(25):
-            for y in range(14):
-                pygame.draw.rect(self.game_surface, (255,255,255), (x*32,y*32,32, 32), 1)
-        pygame.draw.rect(self.game_surface, (255,0,0), ((get_mouse_pos().x / 1.6) // 32 * 32 + 1, (get_mouse_pos().y / 1.6) // 32 * 32 + 1, 30, 30))
-        pygame.draw.circle(self.game_surface, (255,255,255), get_mouse_pos() / 1.6, 10)
-        temp_surf = pygame.transform.scale(self.game_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.screen.blit(temp_surf,(0,0))
-
-
-def get_mouse_pos() -> pygame.Vector2():
-    return pygame.Vector2(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+        self.level_manager.draw()
+        self.screen.blit(pygame.transform.scale(self.level_manager.game_surf,( SCREEN_WIDTH, SCREEN_HEIGHT)), (0,0))
--- game/level.py
+++ game/level.py
@@ -1,58 +1,64 @@
+
 import pygame
 import random
+from misc.constants import *
 from misc.util import *
 
 class Generator():
-    def __init__(self, range_x : tuple[int], range_y : tuple[int]):
-        self.range_x = range_x
-        self.range_y = range_y
+    def __init__(self, screen_size):
+        self.range_x = (0, screen_size[0])
+        self.range_y = (0, screen_size[1])
+        self.direction = pygame.Vector2(1,0)
 
         self.points = []
         self.obsticles = []
 
-        self.direction = pygame.Vector2(0, 1)
-        self.last_direction = pygame.Vector2(0, 1)
+    def generate_level(self, no_turns, no_obsticles):
+        self.points = self.generate_path(no_turns)
+        return Level(self.points)
 
-    def generate_random_path(self, no_turns):
-        current_point = pygame.Vector2(0,random.randrange(self.range_y[0],self.range_y[1]))
-        self.points.append(current_point)
-
-        # start generation
+    def generate_path(self, no_turns):
+        points = [pygame.Vector2(0, random.uniform(self.range_y[0], self.range_y[1]))]
+        current_point = points[-1]
         for i in range(no_turns):
-            new_point = self.get_new_point(current_point)
+            current_point = self.get_point(current_point)
+            points.append(current_point)
 
-            while not in_range(new_point.x, self.range_x) or not in_range(new_point.y, self.range_y):
-                self.direction = self.last_direction
-                new_point = self.get_new_point(current_point)
+        return points
 
-            # add point to the path
-            self.points.append(new_point)
-            current_point = new_point
+    def get_point(self, current_point):
+        direction = self.direction.rotate(random.choice([-90, 90]))
 
-        if self.points[-1].x != self.range_x[1] and self.direction.x == 1:
-            self.points[-1].x = self.range_x[1]
-        elif self.points[-1].x != self.range_x[0] and self.direction.x == -1:
-            self.points[-1].x = self.range_x[0]
-        elif self.points[-1].y != self.range_y[1] and self.direction.y == 1:
-            self.points[-1].y = self.range_y[1]
-        elif self.points[-1].y != self.range_y[0] and self.direction.y == -1:
-            self.points[-1].y = self.range_y[0]
+        final_point = pygame.Vector2(0,0)
 
-        return self.points
+        final_point.x = current_point.x + direction.x * self.range_x[1] // 2 * random.uniform(0.5,1.5)
+        final_point.y = current_point.y + direction.y * self.range_y[1] // 2 * random.uniform(0.5,1.5)
 
+        if not in_range(final_point.x, self.range_x) or not in_range(final_point.y, self.range_y):
+            final_point = self.get_point(current_point)
 
-    def get_new_point(self, current_point):
-        self.last_direction = self.direction
-        self.direction = self.direction.rotate(random.choice([-90, 90]))
+        self.direction = direction
 
-        new_point = pygame.Vector2()
-        new_point.x = current_point.x + self.direction.x * self.range_x[0] / 2 * random.uniform(0.5, 1.5)
-        new_point.y = current_point.y + self.direction.y * self.range_y[0] / 2 * random.uniform(0.5, 1.5)
+        return final_point
 
-        return new_point
+class Level_manager():
+    def __init__(self, scale):
+        self.generator = Generator([SCREEN_WIDTH // scale, SCREEN_HEIGHT // scale])
+        self.game_surf = pygame.surface.Surface((SCREEN_WIDTH // scale, SCREEN_HEIGHT // scale))
+        self.current_level = self.generator.generate_level(6,10)
 
-    def generate_obsticles(self):
-        pass
+    def change_level(self, no_turns, no_obsticles):
+        self.current_level = self.generator.generate_level(no_turns, no_obsticles)
     
-    def check_obsticle(self, obsticle):
-        pass
+    def draw(self):
+        self.current_level.draw(self.game_surf)
+
+class Level():
+    def __init__(self, points) -> None:
+        self.points = points
+        self.obsticles = []
+    
+    def draw(self, surface):
+        surface.fill((0,0,0))
+        for point in self.points:
+            pygame.draw.circle(surface, (255,255,255), point, 2)
