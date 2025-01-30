afa1d2ec50f914f0c4f0b20fc6593045201403bd
lava <oearith@gmail.com>
2025-01-30 10:20:16 +0000

    added refresh button to leaderboard

--- game/screens/leaderboard.py
+++ game/screens/leaderboard.py
@@ -7,9 +7,12 @@ class Leaderboard(Screen):
     def __init__(self, screen_manager) -> None:
         super().__init__(screen_manager)
         self.back_color = (DARK_BACKGROUND_COLOR)
-
+            
+        self.add_item("Title", Label(LABEL_DARK, rect = (50,10,SCREEN_WIDTH,200), text = "LEADERBOARD", positioning="relative", font_size=100)) 
         self.add_item("btn_back", Button(BUTTON_DARK_NO_FILL , rect = (25,25,50,50), text = get_icon_hex("arrow_back"), on_click= self.btn_back_on_click))
         self.add_item("lbl_highscores", Label(LABEL_DARK, rect=(50,50,SCREEN_WIDTH/2,SCREEN_HEIGHT-50),text="",positioning="relative",font_size=30))
+
+        self.add_item("btn_load", Button(BUTTON_DARK, rect=(50,90,180,100), text="refresh", on_click=self.on_open, positioning="relative"))
 
     def update(self):
         if pygame.key.get_just_pressed()[pygame.K_ESCAPE]:
@@ -26,7 +29,7 @@ class Leaderboard(Screen):
         
         for i in range(len(response.json()["names"])):
             scores.append([response.json()["names"][i],response.json()["scores"][i]])
-        scores.sort(key=lambda x: x[1], reverse=True)
+        scores.sort(key=lambda x: int(x[1]), reverse=True)
         for i in range(15):
             if i < len(scores):
                 self.items["lbl_highscores"].text += f'{scores[i][0]} - {scores[i][1]} \n'
